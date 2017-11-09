31731cd2bd5c8cde5cf52f462a82560ba7d68cac
p.leroux <pleroux@student.42.fr>
2017-11-09 15:01:25 +0100

    memccpy

---
+++ ft_memccpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pleroux <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/09 14:02:12 by pleroux           #+#    #+#             */
+/*   Updated: 2017/11/09 14:58:27 by pleroux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
+{
+	unsigned char	*ret;
+	unsigned char	*src_cpy;
+	size_t				i;
+
+	i = 0;
+	ret = (unsigned char*)dest;
+	src_cpy = (unsigned char*)src;
+	while (i < n && (unsigned char)c != src_cpy[i])
+	{
+		ret[i] = src_cpy[i];
+		i++;
+	}
+	ret[i] = (unsigned char)c;
+	return (i == n? NULL:ret + i + 1);
+}
