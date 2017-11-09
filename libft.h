961f297cd05e65ee66fc92548d34538848820ee1
p.leroux <pleroux@student.42.fr>
2017-11-09 13:27:56 +0100

    ft_bzero

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pleroux <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/09 10:12:55 by pleroux           #+#    #+#             */
+/*   Updated: 2017/11/09 13:16:52 by pleroux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <string.h>
+#include <unistd.h>
+
+void		*ft_memset (void *s, int c, size_t n);
+void		ft_bzero(void *s, size_t n);
+
+
+#endif
