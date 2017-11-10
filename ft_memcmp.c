7ede86d32b6c98ea1a2d3cd86b3c493e6c003ca2
p.leroux <pleroux@student.42.fr>
2017-11-10 10:35:49 +0100

    memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pleroux <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/10 10:17:09 by pleroux           #+#    #+#             */
+/*   Updated: 2017/11/10 10:34:16 by pleroux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*ss1;
+	unsigned char	*ss2;
+
+
+	i = 0;
+	ss1 = (unsigned char*)s1;
+	ss2 = (unsigned char*)s2;
+	while (i < n && ss1[i] == ss2[i])
+		i++;
+	return (ss1[i] - ss2[i]);
+}
