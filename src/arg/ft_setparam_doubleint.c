d34578a0eb0bfa652334ada8333d5b694f6f3dbc
p.leroux <pleroux@student.42.fr>
2018-01-03 17:37:01 +0100

    fix buf segv

--- src/arg/ft_setparam_doubleint.c
+++ src/arg/ft_setparam_doubleint.c
@@ -6,7 +6,7 @@
 /*   By: pierre <pleroux@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/19 16:06:03 by pierre            #+#    #+#             */
-/*   Updated: 2017/12/19 17:15:10 by pierre           ###   ########.fr       */
+/*   Updated: 2018/01/03 15:28:26 by pleroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,17 @@ t_bool			ft_setparam_doubleint(t_arg *a, char *s, int *ptr, int data, int i)
 {
 	t_list		*tmp;
 	char		**ss;
+	t_string	str;
 
 	if (ft_lststrfind(a->long_param, &tmp, s))
 	{
-		ss = ft_strsplit(ft_getparam_data(tmp), ',');
+		str = ft_getparam_data(tmp);
+		if (!str)
+		{
+			*ptr = data;
+			return (TRUE);
+		}
+		ss = ft_strsplit(str, ',');
 		if (ss[0] && ss[1])
 			*ptr = ft_atoi(ss[i % 2]);
 		else
--- src/arg/ft_setparam_hex.c
+++ src/arg/ft_setparam_hex.c
@@ -6,7 +6,7 @@
 /*   By: pierre <pleroux@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/19 17:10:59 by pierre            #+#    #+#             */
-/*   Updated: 2017/12/19 17:12:20 by pierre           ###   ########.fr       */
+/*   Updated: 2018/01/03 15:30:41 by pleroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,18 @@
 
 t_bool			ft_setparam_hex(t_arg *a, char *s, int *ptr, int data)
 {
-	t_list *tmp;
-
+	t_list		*tmp;
+	t_string	str;
 	if (ft_lststrfind(a->long_param, &tmp, s))
-		*ptr = ft_atoi_base(ft_getparam_data(tmp), "0123456789ABCDEF");
+	{
+		str = ft_getparam_data(tmp);
+		if (!str)
+		{
+			*ptr = data;
+			return (TRUE);
+		}
+		*ptr = ft_atoi_base(str, "0123456789ABCDEF");
+	}
 	else
 		*ptr = data;
 	return (TRUE);
