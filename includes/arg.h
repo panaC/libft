5a9972728851825c2fc676a58a0f4282f0362dee
p.leroux <pleroux@student.42.fr>
2018-01-09 11:56:17 +0100

    norm atoi_hex

--- includes/arg.h
+++ includes/arg.h
@@ -6,7 +6,7 @@
 /*   By: pierre <pleroux@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 15:37:07 by pierre            #+#    #+#             */
-/*   Updated: 2018/01/09 11:41:18 by pleroux          ###   ########.fr       */
+/*   Updated: 2018/01/09 11:53:40 by pleroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ t_arg			*ft_get_arg(int ac, char **argv);
 t_string		ft_getarg_data(t_list *n);
 t_string		ft_getparam_data(t_list *n);
 t_bool			ft_setparam_int(t_arg *a, char *s, int *ptr, int data);
-t_bool			ft_setparam_doubleint(t_arg *a, char *s, int *ptr, int data );
-t_bool			ft_setparam_hex(t_arg *a, char *s, t_uint32 *ptr, t_uint32 data);
+t_bool			ft_setparam_doubleint(t_arg *a, char *s, int *ptr, int data);
+t_bool			ft_setparam_hex(t_arg *a, char *s, t_uint32 *ptr,
+		t_uint32 data);
 void			ft_freearg(t_arg **a);
 #endif
--- src/conv/ft_atoi_hex.c
+++ src/conv/ft_atoi_hex.c
@@ -6,7 +6,7 @@
 /*   By: pleroux <pleroux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/04 11:17:52 by pleroux           #+#    #+#             */
-/*   Updated: 2018/01/04 21:28:28 by pleroux          ###   ########.fr       */
+/*   Updated: 2018/01/09 11:54:48 by pleroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ t_uint32			ft_atoi_hex(const char *str)
 	size_t			i;
 	char			*s;
 
-	//printf("FT Atoi hex\n");
 	if (!str)
 		return (0);
 	i = 0;
@@ -27,22 +26,17 @@ t_uint32			ft_atoi_hex(const char *str)
 	while (i < ft_strlen(s))
 	{
 		s[i] = ft_toupper(s[i]);
-		//printf("%c\t%ld\n", s[i], i);
 		i++;
 	}
 	i = 0;
-	//printf("%s\n", s);
 	while (ft_isspace(s[i]))
 		i++;
-	//printf("hello %s %ld\n", s, i);
 	if (s[i] == '0' && s[i + 1] && s[i + 1] == 'X' && s[i + 2]
 			&& !ft_isspace(s[i + 2]))
 		i += 2;
 	else
 		return (0);
-	//printf("hello %s %ld\n", s, i);
 	res = (t_uint32)ft_atoi_base(s + i, "0123456789ABCDEF");
-	//printf("hello %s\t%x\n", s, res);
 	ft_memdel((void**)&(s));
 	return (res);
 }
