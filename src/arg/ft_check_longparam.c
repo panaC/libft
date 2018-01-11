7c1ce96fa5d1e3e87a0a73bbc3cd48617865fa3b
p.leroux <pleroux@student.42.fr>
2018-01-11 19:57:28 +0100

    leaks arg

--- src/arg/ft_check_longparam.c
+++ src/arg/ft_check_longparam.c
@@ -6,7 +6,7 @@
 /*   By: pleroux <pleroux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/09 15:41:36 by pleroux           #+#    #+#             */
-/*   Updated: 2018/01/09 19:34:52 by pleroux          ###   ########.fr       */
+/*   Updated: 2018/01/11 11:34:56 by pleroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ t_bool			ft_check_longparam(t_list *arg, t_list *src, t_bool del)
 	size_t		nb;
 
 	nb = 0;
-	ft_putlst(arg);
 	while (src)
 	{
 		if (ft_search_param(arg, NULL, (t_string)src->content))
--- src/arg/ft_setparam_doubleint.c
+++ src/arg/ft_setparam_doubleint.c
@@ -6,7 +6,7 @@
 /*   By: pierre <pleroux@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/19 16:06:03 by pierre            #+#    #+#             */
-/*   Updated: 2018/01/09 19:38:10 by pleroux          ###   ########.fr       */
+/*   Updated: 2018/01/11 17:43:19 by pleroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ t_bool			ft_setparam_doubleint(t_arg *a, t_string s, int *ptr, int data)
 	t_string	*ss;
 	t_string	str;
 	static int	i = 0;
+	int			j;
 
+	j = 0;
 	if (!(a || s || ptr))
 		return (FALSE);
 	*ptr = data;
@@ -29,8 +31,12 @@ t_bool			ft_setparam_doubleint(t_arg *a, t_string s, int *ptr, int data)
 			ss = ft_strsplit(ft_split_longparam(str, 1, TRUE), ',');
 			if (ss[0] && ss[1])
 				*ptr = (ft_atoi(ss[i % 2]) < 3000) ? ft_atoi(ss[i % 2]) : data;
+			while (ss[j])
+				ft_memdel((void**)&(ss[j++]));
+			ft_memdel((void**)ss);
 		}
 		i = (!i) & 0x1;
+		ft_memdel((void**)&str);
 	}
 	return (TRUE);
 }
