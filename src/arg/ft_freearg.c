1e7dd8dc9c8591a2a215d5015713b5def099de5a
pleroux <p.leroux@gmx.com>
2018-02-15 18:47:59 +0100

    unused var

--- src/arg/ft_freearg.c
+++ src/arg/ft_freearg.c
@@ -6,7 +6,7 @@
 /*   By: pierre <pleroux@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/21 00:42:09 by pierre            #+#    #+#             */
-/*   Updated: 2018/01/03 13:13:14 by pleroux          ###   ########.fr       */
+/*   Updated: 2018/02/15 11:01:14 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <string.h>
 #include <arg.h>
 
-static void		del(void *c, size_t s)
+static void		del(void *c, __attribute__((unused)) size_t s)
 {
 	s = 0;
 	ft_memdel((void**)&c);
