bd445154df0712da5d2a9270a13218f521f303bb
p.leroux <pleroux@student.42.fr>
2018-03-15 13:42:21 +0100

    add strnjoin

---
+++ src/str/ft_strnjoin.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnjoin.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pleroux <pleroux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/01 11:31:28 by pleroux           #+#    #+#             */
+/*   Updated: 2018/02/02 10:19:24 by pleroux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/libft.h"
+
+char		*ft_strnjoin(char const *s1, size_t n1, char const *s2, size_t n2)
+{
+	char		*ret;
+
+	if (!s1 || !s2)
+		return (NULL);
+	ret = ft_strnew(n1 + n2 + 1);
+	if (!ret)
+		return (NULL);
+	ft_memcpy(ret, s1, n1);
+	ft_memcpy(ret + n1, s2, n2);
+	ret[n1 + n2] = '\0';
+	return (ret);
+}
